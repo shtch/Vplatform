5eec3abf25f69a17ff1e69d84a2dbb5fe3ce2394
shtch <oleg.shcherbatykh@mail.ru>
2020-04-09 23:40:38 +0300

    fix pwa 2

--- src/main/java/ru/vapp/AppShell.java
+++ src/main/java/ru/vapp/AppShell.java
@@ -8,6 +8,10 @@ import com.vaadin.flow.server.PWA;
 @PWA(name = "VPlatform Application",
         shortName = "VPlatform",
         description = "This is an example Vaadin application.",
-        enableInstallPrompt = true)
+        enableInstallPrompt = false,
+		startPath = "login",
+		backgroundColor = "#227aef", themeColor = "#227aef",
+		offlinePath = "offline-page.html",
+		offlineResources = {"images/offline-login-banner.jpg"})
 public class AppShell implements AppShellConfigurator {
 }
